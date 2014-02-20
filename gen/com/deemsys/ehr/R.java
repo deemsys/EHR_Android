eedfc831553f836c90b9d6d3aa72261eb0aafebb
deemsys <sangeetha_d@deemsysinc.com>
2014-02-20 02:53:25 -0800

    Drawable images added for spalshscreen and logo.

--- gen/com/deemsys/ehr/R.java
+++ gen/com/deemsys/ehr/R.java
@@ -17,25 +17,67 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int admin=0x7f020000;
+        public static final int doctor=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int icon_admin_tab=0x7f020003;
+        public static final int icon_doctor_tab=0x7f020004;
+        public static final int icon_patient_tab=0x7f020005;
+        public static final int logbg=0x7f020006;
+        public static final int loginbg=0x7f020007;
+        public static final int logintext=0x7f020008;
+        public static final int patient=0x7f020009;
+        public static final int registbg=0x7f02000a;
+        public static final int shape=0x7f02000b;
+        public static final int splashscreen=0x7f02000c;
+        public static final int tabback=0x7f02000d;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int Leditpw=0x7f080003;
+        public static final int Ledituname=0x7f080001;
+        public static final int Lpass=0x7f080002;
+        public static final int Luname=0x7f080000;
+        public static final int action_settings=0x7f08000e;
+        public static final int buttonCreateAccount=0x7f08000c;
+        public static final int editTextConfirmPassword=0x7f08000b;
+        public static final int editTextPassword=0x7f08000a;
+        public static final int editTextUserName=0x7f080008;
+        public static final int errorlog=0x7f080006;
+        public static final int login=0x7f080005;
+        public static final int register=0x7f08000d;
+        public static final int registeremail=0x7f080009;
+        public static final int relativeLayout1=0x7f080004;
+        public static final int release=0x7f080007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int adminlogin_layout=0x7f030001;
+        public static final int dialogbox=0x7f030002;
+        public static final int doctor_regist=0x7f030003;
+        public static final int doctorlogin_layout=0x7f030004;
+        public static final int patient_regist=0x7f030005;
+        public static final int patientlogin_layout=0x7f030006;
+        public static final int splashscreen=0x7f030007;
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int button=0x7f060008;
+        public static final int close=0x7f060009;
+        public static final int don_t_have_an_account_register_here_=0x7f060007;
+        public static final int hello_world=0x7f060002;
+        public static final int login=0x7f060006;
+        public static final int password=0x7f060004;
+        public static final int register=0x7f060005;
+        public static final int release=0x7f06000a;
+        public static final int username=0x7f060003;
     }
     public static final class style {
         /** 
@@ -59,10 +101,14 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f040003;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f040004;
+        public static final int CustomFontStyle=0x7f040000;
+        public static final int CustomFontStyle1=0x7f040001;
+        public static final int SpecialText=0x7f040002;
+        public static final int button_text=0x7f040005;
     }
 }
--- src/com/deemsys/ehr/MainActivity.java
+++ src/com/deemsys/ehr/MainActivity.java
@@ -1,15 +1,53 @@
 package com.deemsys.ehr;
 
 import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.view.Menu;
+import android.app.TabActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
-public class MainActivity extends Activity {
+@SuppressWarnings("deprecation")
+public class MainActivity extends TabActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActionBar actbar=getActionBar();
+	    actbar.show();
+	    actbar.setSubtitle("Electronic Healthcare record");
         setContentView(R.layout.activity_main);
+        
+        
+        
+        TabHost tabHost = getTabHost();
+        
+        // Tab for Photos
+        TabSpec adminspec = tabHost.newTabSpec("Admin");
+        // setting Title and Icon for the Tab
+        adminspec.setIndicator("Admin Login", getResources().getDrawable(R.drawable.icon_admin_tab));
+        Intent adminIntent = new Intent(this, adminlogin.class);
+        adminspec.setContent(adminIntent);
+         
+        // Tab for Songs
+        TabSpec doctorspec = tabHost.newTabSpec("Doctor");        
+        doctorspec.setIndicator("Doctor Login", getResources().getDrawable(R.drawable.icon_doctor_tab));
+        Intent doctorIntent = new Intent(this, doctorlogin.class);
+        doctorspec.setContent(doctorIntent);
+         
+        // Tab for Videos
+        TabSpec patientspec = tabHost.newTabSpec("Patient");
+        patientspec.setIndicator("Patient Login", getResources().getDrawable(R.drawable.icon_patient_tab));
+        Intent patientIntent = new Intent(this, patientlogin.class);
+        patientspec.setContent(patientIntent);
+         
+        // Adding all TabSpec to TabHost
+        tabHost.addTab(adminspec); // Adding admin tab
+        tabHost.addTab(doctorspec); // Adding doctor tab
+        tabHost.addTab(patientspec); // Adding patient tab
     }
 
 
